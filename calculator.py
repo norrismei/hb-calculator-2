2a7ba80ef6285f917572ef39da26b4cc6e84f16a
Norris Mei <norrismei3@gmail.com>
2020-07-09 16:16:40 +0000

    Removed extra print statements. Added input message.

--- calculator.py
+++ calculator.py
@@ -5,7 +5,7 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
 
 def main():
     while True:
-        input_string = input()
+        input_string = input("Enter equation: ")
         tokens = input_string.split()
         if tokens[0] == "q":
             break
@@ -14,30 +14,21 @@ def main():
             answer = add(float(tokens[1]), float(tokens[2]))
         elif operator == "-":
             answer = subtract(float(tokens[1]), float(tokens[2]))
-            print(answer)
         elif operator == "*":
             answer = multiply(float(tokens[1]), float(tokens[2]))
-            print(answer)
         elif operator == "/":
             answer = divide(float(tokens[1]), float(tokens[2]))
-            print(answer)
         elif operator == "square":
             answer = square(float(tokens[1]))
-            print(answer)
         elif operator == "cube":
             answer = cube(float(tokens[1]))
-            print(answer)
         elif operator == "pow":
             answer = power(float(tokens[1]), float(tokens[2]))
-            print(answer)
         elif operator == "mod":
             answer = mod(float(tokens[1]), float(tokens[2]))
-            print(answer)
-        else:
+        else: #If operator doesn't match any of the above, then it's invalid.
             print("That's not a valid operator. Try again.")
             continue
         print(answer)
 
 main()
-
-# Replace this with your code
